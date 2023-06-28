67e0e7ac04e410a85f33e8b5730274a9f69efc6b
ngmtine <ngmtine@gmail.com>
2023-06-28 23:59:57 +0900

    tweak

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,18 @@
 import BlogPost from "./post/[slug]/page";
 import fetchPostList from "@/util/fetchPostList";
 
-export default function Home() {
-    const posts = fetchPostList().slice(0, 5);
+const posts = fetchPostList().slice(0, 5);
 
-    const postItems = posts.map((post) => {
-        return (
-            <div key={post.slug}>
-                <BlogPost params={{ ...post }} />
-            </div>
-        );
-    });
-
+const postItems = posts.map((post) => {
     return (
-        <div className="p-5 pt-20">
-            <div>{postItems}</div>
+        <div key={post.slug}>
+            <BlogPost params={{ ...post }} />
         </div>
     );
+});
+
+export default function Home() {
+    return <div className="p-5 pt-20">{postItems}</div>;
 }
 
 // pages.jsxが実際に表示されるページです。従来のPages Routerではpagesフォルダ内の.jsxファイルは全てページとして解釈されましたが、App Routerではpage.jsxのみがページとして解釈されます。
--- src/app/post/[slug]/layout.tsx
+++ src/app/post/[slug]/layout.tsx
@@ -1,5 +1,3 @@
-// import "./globals.css";
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return <div className="p-5 pt-20">{children}</div>;
 }
