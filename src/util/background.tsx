722c85f6646c4c57593b45499b8817ee29be50ee
ngmtine <ngmtine@gmail.com>
2023-07-08 03:51:36 +0900

    wip: leftもランダム化

--- src/util/background.tsx
+++ src/util/background.tsx
@@ -4,10 +4,16 @@ const randomTop = (() => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 })();
 
+const randomLeft = (() => {
+    const min = 0;
+    const max = 70;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+})();
+
 const style = {
     position: "fixed",
     top: `${randomTop}vh`,
-    left: 0,
+    left: `${randomLeft}vh`,
     width: "100vw",
     height: "100vh",
     zIndex: -1,
