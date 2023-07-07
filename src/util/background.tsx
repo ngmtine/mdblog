ceb2ba3ecc027b953b4bf2e920275dbefab77222
ngmtine <ngmtine@gmail.com>
2023-07-08 03:48:24 +0900

    wip: styled-components やめて生style直書きに変更

--- src/util/background.tsx
+++ src/util/background.tsx
@@ -1,29 +1,23 @@
-"use client";
-
-import styled from "styled-components";
-
 const randomTop = (() => {
     const min = -30;
     const max = 70;
     return Math.floor(Math.random() * (max - min + 1)) + min;
 })();
 
-const DynamicBackground = styled.svg.attrs({
-    xmlns: "http://www.w3.org/2000/svg",
-})`
-    position: fixed;
-    top: ${randomTop}vh;
-    left: 0;
-    width: 100vw;
-    height: 100vh;
-    z-index: -1;
-`;
+const style = {
+    position: "fixed",
+    top: `${randomTop}vh`,
+    left: 0,
+    width: "100vw",
+    height: "100vh",
+    zIndex: -1,
+} as const;
 
 const Background = () => {
     return (
-        <DynamicBackground>
+        <svg xmlns="http://www.w3.org/2000/svg" style={style}>
             <circle cx="300" cy="300" r="100" fill="blue" />
-        </DynamicBackground>
+        </svg>
     );
 };
 
