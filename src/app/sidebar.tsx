e8ce52a2cc803d4aca64e99f9e59deae46396232
ngmtine <ngmtine@gmail.com>
2023-07-08 17:22:16 +0900

    update: ハンバーガーボタン一旦なし

--- src/app/sidebar.tsx
+++ src/app/sidebar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import HamburgerButton from "@/util/hamburgerButton";
+// import HamburgerButton from "@/util/hamburgerButton";
 
 const Sidebar = ({ children }) => {
     const [isOpen, setIsOpen] = useState(false);
@@ -14,9 +14,7 @@ const Sidebar = ({ children }) => {
     return (
         <div id="leftSideBar" className="w-64 p-4 h-screen overflow-y-scroll overflow-x-hidden pt-20 hidden md:block">
             {children}
-            <div className="fixed bottom-0">
-                <HamburgerButton callback={toggleOpen}></HamburgerButton>
-            </div>
+            <div className="fixed bottom-0">{/* <HamburgerButton callback={toggleOpen}></HamburgerButton> */}</div>
         </div>
     );
 };
