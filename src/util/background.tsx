ecc368c1ca05190470c1ab43ddf41e096a5a0ae3
ngmtine <ngmtine@gmail.com>
2023-07-08 14:37:02 +0900

    wip: ランダム値をいい感じに

--- src/util/background.tsx
+++ src/util/background.tsx
@@ -13,14 +13,15 @@ const style = {
 
 const Background = () => {
     const cx = `${getRandInt()}%`;
-    const cy = `${getRandInt({ max: 30 })}%`;
+    const cy = `${getRandInt({ min: 10, max: 30 })}%`;
     const rx = `${getRandInt({ min: 500, max: 600 })}px`;
-    const ry = `${getRandInt({ min: 200, max: 300 })}px`;
+    const ry = `${getRandInt({ min: 70, max: 100 })}px`;
     const rotate = `rotate(${getRandInt({ min: -30, max: 30 })})`;
 
     return (
         <svg xmlns="http://www.w3.org/2000/svg" style={style}>
-            <ellipse cx={cx} cy={cy} rx={rx} ry={ry} fill="rgba(0, 0, 255, 0.1)" filter="blur(2rem)" transform={rotate} />
+            <ellipse cx={cx} cy={cy} rx={rx} ry={ry} fill="rgba(0, 0, 255, 0.02)" filter="blur(2rem) drop-shadow(100px 100px 2px blue)" transform={rotate} />
+            <ellipse cx={cx} cy={cy} rx={rx} ry={ry} fill="rgba(0, 0, 255, 0.02)" filter="blur(2rem) drop-shadow(-100px -100px 2px red)" transform={rotate} />
         </svg>
     );
 };
