5f1c0bd532bf31051dcf394c3ab8385b63a5b807
ngmtine <ngmtine@gmail.com>
2023-07-08 13:48:35 +0900

    wip: リロードのたびに位置変更

--- src/util/background.tsx
+++ src/util/background.tsx
@@ -1,19 +1,9 @@
-const randomTop = (() => {
-    const min = -30;
-    const max = 70;
+const getRandInt = ({ min = 0, max = 100 } = {}) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-})();
-
-const randomLeft = (() => {
-    const min = 0;
-    const max = 70;
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-})();
+};
 
 const style = {
     position: "fixed",
-    // top: `${randomTop}vh`,
-    // left: `${randomLeft}vh`,
     top: 0,
     left: 0,
     width: "100vw",
@@ -22,9 +12,11 @@ const style = {
 } as const;
 
 const Background = () => {
+    const cx = `${getRandInt()}%`;
+    const cy = `${getRandInt()}%`;
     return (
         <svg xmlns="http://www.w3.org/2000/svg" style={style}>
-            <ellipse cx="300" cy="300" rx="100" ry="30" fill="rgba(0, 0, 255, 0.3)" filter="blur(1.5rem)" />
+            <ellipse cx={cx} cy={cy} rx="100" ry="30" fill="rgba(0, 0, 255, 0.3)" filter="blur(1.5rem)" />
         </svg>
     );
 };
