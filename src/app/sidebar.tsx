01b1d59532157f752ba4f4e9e518128b1c92226a
ngmtine <ngmtine@gmail.com>
2023-07-08 16:57:34 +0900

    update: レスポンシブ対応

--- src/app/sidebar.tsx
+++ src/app/sidebar.tsx
@@ -12,7 +12,7 @@ const Sidebar = ({ children }) => {
 
     // children: PostList
     return (
-        <div id="leftSideBar" className="w-64 p-4 h-screen overflow-y-scroll overflow-x-hidden pt-20">
+        <div id="leftSideBar" className="w-64 p-4 h-screen overflow-y-scroll overflow-x-hidden pt-20 hidden md:block">
             {children}
             <div className="fixed bottom-0">
                 <HamburgerButton callback={toggleOpen}></HamburgerButton>
