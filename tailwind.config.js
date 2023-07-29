601bf72119b795a33a8017e27870bb467f7bfcb4
ngmtine <ngmtine@gmail.com>
2023-07-29 11:22:26 +0900

    change: container クラスのブレークポイントを2xlのみに限定

--- tailwind.config.js
+++ tailwind.config.js
@@ -37,8 +37,8 @@ module.exports = {
                 screens: {
                     // sm: "640px",
                     // md: "768px",
-                    lg: "1024px",
-                    xl: "1280px",
+                    // lg: "1024px",
+                    // xl: "1280px",
                     "2xl": "1536px",
                 },
             },
