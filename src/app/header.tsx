61f5d5cd10eec6c5d326d65dc3642f1daa24d965
ngmtine <ngmtine@gmail.com>
2023-07-29 11:51:22 +0900

    change: レスポンシブのヘッダスタイル調整

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -5,7 +5,7 @@ const blogTitle = "真・フランス料理紹介ブログ";
 const Header = () => {
     return (
         <div id="header" className="h-14 z-10 w-full backdrop-blur-sm text-gray-900 dark:text-gray-300 flex items-center fixed">
-            <div className="ml-10 sm:ml-0">
+            <div className="ml-5 md:ml-3 2xl:ml-0">
                 <Link href={"/"} className="text-xl font-semibold mb-5">
                     {blogTitle}
                 </Link>
