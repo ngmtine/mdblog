2a309052f9715a5ae0839d406412c29739c71b8e
ngmtine <ngmtine@gmail.com>
2023-07-30 16:12:39 +0900

    update: ハンバーガーボタンサイズ調整

--- src/util/svgHamburgerButton.tsx
+++ src/util/svgHamburgerButton.tsx
@@ -10,7 +10,7 @@ const SvgHamburgerButton = ({ callback }: SvgHamburgerButtonProps) => {
     return (
         <div>
             <button onClick={callback}>
-                <svg className="text-gray-800 dark:text-white backdrop-blur-sm" style={inlineStyle} xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" viewBox="0 0 17 13" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                <svg className="text-gray-800 dark:text-white backdrop-blur-sm" style={inlineStyle} xmlns="http://www.w3.org/2000/svg" width="1.8rem" height="1.8rem" viewBox="0 0 17 13" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
                     <path d="M1 1h15M1 7h15M1 13h15" />
                 </svg>
             </button>
