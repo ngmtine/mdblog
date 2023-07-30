de255d030542061868ca77b88e16b32c8208e64e
ngmtine <ngmtine@gmail.com>
2023-07-30 14:21:50 +0900

    refactor: コード整理してコメント追加

--- src/app/sidebar.tsx
+++ src/app/sidebar.tsx
@@ -10,77 +10,68 @@ interface Props {
 const Sidebar = ({ children }: Props) => {
     if (typeof window === "undefined") throw new Error();
 
+    // 現在のウィンドウサイズ
     const [windowSize, setWindowSize] = useState({
         width: window.innerWidth,
         height: window.innerHeight,
     });
 
+    // 直前のウィンドウサイズ
     const [prevWindowSize, setPrevWindowSize] = useState({
         width: window.innerWidth,
         height: window.innerHeight,
     });
 
-    // useEffect は以下のような引数を持ちます。
-    // useEffect(callback[, dependencies]);
-    // 第一引数には副作用の処理を記述するためのコールバック関数を持ち、これはレンダーの結果が画面に反映された後に動作します。
-    // 第二引数には依存先の変数が格納される配列が渡されます。これによって副作用の処理をどのタイミングで実行するか決めることができます。
-    // 注）useEffectを使用せず、コンポーネントに直書きすると、レンダリングが発生するたびに直書きした処理が実行される
+    // サイドバーの表示状態を表すブール値
+    const [isSidebarHidden, setIsSidebarHidden] = useState(true);
 
-    // useEffectを使用して、ウィンドウサイズが変更されたときにその値を更新するイベントリスナーを設定しています。具体的には、window.addEventListener("resize", handleResize);によって、resizeイベントが発生したとき（つまりウィンドウがリサイズされたとき）にhandleResize関数を呼び出すようにしています。このhandleResize関数はウィンドウの新しいサイズを取得し、それをwindowSize状態にセットします。
+    // サイドバーに付与するtailwindクラス
+    const [displayClass, setDisplayClass] = useState("block");
 
-    // useEffectが受け取る第一引数の関数は、Reactコンポーネントが初めてレンダリングされた後、または依存配列に含まれる状態/プロップスが変更された後に呼ばれます。この関数は"副作用関数"とも呼ばれます。
-    // 副作用関数内でhandleResize関数を定義しています。この関数はウィンドウの現在のサイズを取得し、setWindowSizeを使ってwindowSize状態を更新します。
-    // window.addEventListener("resize", handleResize);は、ウィンドウサイズが変更されたときにhandleResize関数が呼ばれるように設定しています。
-    // useEffectの第二引数にあたる配列は、"依存配列"と呼ばれます。ここに指定した値が変更されたときだけ、副作用関数が再実行されます。この配列が空（[]）の場合、副作用関数はコンポーネントの初回レンダリング時にのみ実行されます。上記のコードでは、ウィンドウサイズが変更されるたびにウィンドウサイズを取得する必要があるので、依存配列は空にしています。
+    // リサイズ時に呼ばれるコールバック関数 条件に応じてsetDisplayClassを呼ぶ
+    const toggleSidebarStateWithResize = () => {
+        setPrevWindowSize({
+            width: windowSize.width,
+            height: windowSize.height,
+        });
 
+        setWindowSize({
+            width: window.innerWidth,
+            height: window.innerHeight,
+        });
+
+        const threshold = 768;
+        const isWindowExpanding = prevWindowSize.width < threshold && threshold <= windowSize.width;
+        const isWindowShrinking = prevWindowSize.width > threshold && threshold >= windowSize.width;
+
+        if (isWindowExpanding) setDisplayClass("block");
+        if (isWindowShrinking) setDisplayClass("hidden");
+
+        setIsSidebarHidden(true);
+    };
+
+    // クリック時に呼ばれるコールバック関数 条件に応じてsetDisplayClassを呼ぶ
+    const toggleSidebarStateWithClick = () => {
+        const isNarrow = window.innerWidth < 768;
+
+        if (isNarrow && isSidebarHidden) setDisplayClass("block");
+        if (!isNarrow && isSidebarHidden) setDisplayClass("hidden");
+
+        setIsSidebarHidden(!isSidebarHidden);
+    };
+
+    // windowSizeを監視し、コンポーネントが再レンダリングされる度にイベントリスナーを再登録する
     useEffect(() => {
-        const toggleSidebarStateWithResize = () => {
-            const threshold = 768;
-            const isWindowExpanding = prevWindowSize.width < threshold && threshold <= windowSize.width;
-            const isWindowShrinking = prevWindowSize.width > threshold && threshold >= windowSize.width;
+        window.addEventListener("resize", toggleSidebarStateWithResize);
 
-            if (isWindowExpanding) setDisplayClass("block");
-            if (isWindowShrinking) setDisplayClass("hidden");
-        };
-
-        const handleResize = () => {
-            setPrevWindowSize({
-                width: windowSize.width,
-                height: windowSize.height,
-            });
-
-            setWindowSize({
-                width: window.innerWidth,
-                height: window.innerHeight,
-            });
-
-            toggleSidebarStateWithResize();
-        };
-
-        window.addEventListener("resize", handleResize);
-
-        // コンポーネントのアンマウント時にイベントリスナーをクリーンアップします
-        // useEffectのクリーンアップ関数（return文で指定した関数）では、コンポーネントがアンマウントされる際（つまり画面から消える際）にイベントリスナーを削除しています。これは、不要になったイベントリスナーがメモリを消費し続けることを防ぐためです。
+        // useEffectのクリーンアップ関数（return文で指定した関数）は、コンポーネントがアンマウントされる際（つまり画面から消える際）にイベントリスナーを削除する
         return () => {
-            window.removeEventListener("resize", handleResize);
+            window.removeEventListener("resize", toggleSidebarStateWithResize);
         };
 
         // prevWindowSizeが変更されるとき、windowSizeも必ず変更されているため、prevWindowSizeは監視する必要はなく、その警告の抑制
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [windowSize]);
-
-    const [isClicked, setIsClickedButton] = useState(true);
-
-    const toggleSidebarStateWithClick = () => {
-        const isNarrow = window.innerWidth < 768;
-
-        if (isNarrow && isClicked) setDisplayClass("block");
-        if (!isNarrow && isClicked) setDisplayClass("hidden");
-
-        setIsClickedButton(!isClicked);
-    };
-
-    const [displayClass, setDisplayClass] = useState("block");
 
     return (
         <div className={`w-64 p-4 h-screen overflow-y-scroll overflow-x-hidden pt-20 block ${displayClass}`}>
@@ -91,3 +82,12 @@ const Sidebar = ({ children }: Props) => {
 };
 
 export default Sidebar;
+
+// useEffect は以下のような引数を持ちます。
+// useEffect(callback[, dependencies]);
+// 第一引数には副作用の処理を記述するためのコールバック関数を持ち、これはレンダーの結果が画面に反映された後に動作します。
+// 第二引数には依存先の変数が格納される配列が渡されます。これによって副作用の処理をどのタイミングで実行するか決めることができます。
+// この配列が空（[]）の場合、副作用関数はコンポーネントの初回レンダリング時にのみ実行されます。
+// 注）useEffectを使用せず、コンポーネントに直書きすると、レンダリングが発生するたびに直書きした処理が実行される
+
+// useEffectを使用して、ウィンドウサイズが変更されたときにその値を更新するイベントリスナーを設定しています。具体的には、window.addEventListener("resize", toggleSidebarStateWithResize);によって、resizeイベントが発生したとき（つまりウィンドウがリサイズされたとき）にtoggleSidebarStateWithResize関数を呼び出すようにしています。このtoggleSidebarStateWithResize関数はウィンドウの新しいサイズを取得し、それをwindowSize状態にセットします。
