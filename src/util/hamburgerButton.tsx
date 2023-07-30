1821b674aa8722dc315e7809b29c2718c3af402d
ngmtine <ngmtine@gmail.com>
2023-07-30 15:42:39 +0900

    update: ハンバーガーボタン ブラー追加

--- src/util/hamburgerButton.tsx
+++ src/util/hamburgerButton.tsx
@@ -10,7 +10,7 @@ const HamburgerButton = ({ callback }: HamburgerButtonProps) => {
     return (
         <>
             <button onClick={callback}>
-                <svg className="text-gray-800 dark:text-white" style={inlineStyle} xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" viewBox="0 0 17 14" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                <svg className="text-gray-800 dark:text-white backdrop-blur-sm" style={inlineStyle} xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" viewBox="0 0 17 13" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
                     <path d="M1 1h15M1 7h15M1 13h15" />
                 </svg>
             </button>
