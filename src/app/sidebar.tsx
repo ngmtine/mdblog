c3c88ade83ebdda9aea675b0cd5ed832cad77553
ngmtine <ngmtine@gmail.com>
2023-08-06 14:48:47 +0900

    update: クリックされたエレメントがサイドバーの外部であれば、サイドバーを隠す

--- src/app/sidebar.tsx
+++ src/app/sidebar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect, ReactNode } from "react";
+import { useState, useEffect, useRef, ReactNode } from "react";
 import SvgHamburgerButton from "@/util/svgHamburgerButton";
 
 interface Props {
@@ -11,6 +11,7 @@ const Sidebar = ({ children }: Props) => {
     if (typeof window === "undefined") throw new Error();
 
     const threshold = 768;
+    const sidebarRef = useRef(null);
 
     // 現在のウィンドウサイズ
     const [windowSize, setWindowSize] = useState({
@@ -45,19 +46,12 @@ const Sidebar = ({ children }: Props) => {
         const isWindowExpanding = prevWindowSize.width < threshold && threshold <= windowSize.width;
         const isWindowShrinking = prevWindowSize.width > threshold && threshold >= windowSize.width;
 
-        if (isWindowExpanding) {
-            console.log("toggleSidebarStateWithResize: isWindowExpanding");
-            setDisplayClass("block");
-        }
-        if (isWindowShrinking) {
-            console.log("toggleSidebarStateWithResize: isWindowShrinking");
-            setDisplayClass("hidden");
-        }
+        if (isWindowExpanding) setDisplayClass("block");
+        if (isWindowShrinking) setDisplayClass("hidden");
     };
 
     // クリック時に呼ばれるコールバック関数 条件に応じてsetDisplayClassを呼ぶ
     const toggleSidebarStateWithClick = () => {
-        console.log("toggleSidebarStateWithClick");
         if (window.innerWidth < threshold) {
             setDisplayClass(displayClass === "hidden" ? "block" : "hidden");
         } else {
@@ -65,35 +59,39 @@ const Sidebar = ({ children }: Props) => {
         }
     };
 
+    // クリックされたエレメントがサイドバーの外部であれば、サイドバーを隠す
+    const handleDocumentClick = (event: MouseEvent) => {
+        if (threshold <= window.innerWidth) return;
+
+        if (sidebarRef.current && !sidebarRef.current.contains(event.target as Node)) {
+            setDisplayClass("hidden");
+        }
+    };
+
     // windowSizeを監視し、コンポーネントが再レンダリングされる度にイベントリスナーを再登録する
     useEffect(() => {
         window.addEventListener("resize", toggleSidebarStateWithResize);
+        document.addEventListener("click", handleDocumentClick);
 
         return () => {
             window.removeEventListener("resize", toggleSidebarStateWithResize);
+            document.removeEventListener("click", handleDocumentClick);
         };
 
         // prevWindowSizeが変更されるとき、windowSizeも必ず変更されているため、prevWindowSizeは監視する必要はなく、その警告の抑制
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [windowSize]);
+    }, [windowSize, displayClass]);
 
     return (
         <>
             <div className="fixed ml-3 bottom-3">
                 <SvgHamburgerButton callback={toggleSidebarStateWithClick} />
             </div>
-            <div className={`w-64 p-4 h-screen overflow-y-scroll overflow-x-hidden pt-20 block ${displayClass}`}>{children}</div>
+            <div ref={sidebarRef} className={`w-64 p-4 h-screen overflow-y-scroll overflow-x-hidden pt-20 block ${displayClass}`}>
+                {children}
+            </div>
         </>
     );
 };
 
 export default Sidebar;
-
-// useEffect は以下のような引数を持ちます。
-// useEffect(callback[, dependencies]);
-// 第一引数には副作用の処理を記述するためのコールバック関数を持ち、これはレンダーの結果が画面に反映された後に動作します。
-// 第二引数には依存先の変数が格納される配列が渡されます。これによって副作用の処理をどのタイミングで実行するか決めることができます。
-// この配列が空（[]）の場合、副作用関数はコンポーネントの初回レンダリング時にのみ実行されます。
-// 注）useEffectを使用せず、コンポーネントに直書きすると、レンダリングが発生するたびに直書きした処理が実行される
-
-// useEffectを使用して、ウィンドウサイズが変更されたときにその値を更新するイベントリスナーを設定しています。具体的には、window.addEventListener("resize", toggleSidebarStateWithResize);によって、resizeイベントが発生したとき（つまりウィンドウがリサイズされたとき）にtoggleSidebarStateWithResize関数を呼び出すようにしています。このtoggleSidebarStateWithResize関数はウィンドウの新しいサイズを取得し、それをwindowSize状態にセットします。
