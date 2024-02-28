d416648accfa1d52248e24c164f01b7777896bda
ngmtine <ngmfries@gmail.com>
2024-02-29 04:03:26 +0900

    fix next: サイドバー表示閾値を1024pxに変更 ただしスマホ等でサイドバー必要なのでサイドバー開閉ボタンあとで戻す必要あり

--- next/app/Sidebar.tsx
+++ next/app/Sidebar.tsx
@@ -6,7 +6,7 @@ export const Sidebar = () => {
     return (
         <div
             id={"sidebar"}
-            className="fixed left-0 top-0 z-10 h-full w-80 -translate-x-full bg-iceberg-light p-4 text-gray-900 opacity-95 transition-transform duration-300 ease-in-out dark:bg-iceberg-dark dark:text-gray-300 md:translate-x-0"
+            className="fixed left-0 top-0 z-10 h-full w-80 -translate-x-full bg-iceberg-light p-4 text-gray-900 opacity-95 transition-transform duration-300 ease-in-out dark:bg-iceberg-dark dark:text-gray-300 lg:translate-x-0"
         >
             <ul className="menu min-h-full">
                 <li>
