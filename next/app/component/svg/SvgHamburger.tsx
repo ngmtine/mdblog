d3a4c615c6d6a79ac8b6c82c92340614e996c034
ngmtine <ngmfries@gmail.com>
2024-03-22 19:13:31 +0900

    fix: ハンバーガー若干細く

--- next/app/component/svg/SvgHamburger.tsx
+++ next/app/component/svg/SvgHamburger.tsx
@@ -10,7 +10,7 @@ export const SvgHamburger = () => {
             stroke="currentColor"
             strokeLinecap="round"
             strokeLinejoin="round"
-            strokeWidth="2"
+            strokeWidth="1.8"
         >
             <path d="M1 1h15M1 7h15M1 13h15" />
         </svg>
