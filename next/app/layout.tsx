62c92f3ec84b8b884bf0b6236e8164adecbb9844
ngmtine <ngmfries@gmail.com>
2024-03-23 15:24:53 +0900

    change: warningでるの承知でmetadataをlayout.tsxに戻した

--- next/app/layout.tsx
+++ next/app/layout.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Metadata } from "next";
+
 import { Sidebar } from "@/app/_component/Sidebar";
 
 import Background from "./_component/background";
@@ -8,6 +10,31 @@ import { ToggleSidebarButton } from "./_component/ToggleSidebarButton";
 import { ThemeProvider } from "./ThemeProvider";
 
 import "./globals.css";
+
+const siteName = "mdblog";
+const description = "お料理ブログです";
+const author = "ngmtine";
+
+export const metadata: Metadata = {
+    metadataBase: new URL("http://localhost:3000"),
+    title: siteName,
+    description: description,
+    openGraph: {
+        title: siteName,
+        description: description,
+        url: "https://www.ngmtine.blog",
+        siteName: siteName,
+        locale: "ja_JP",
+        type: "website",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: siteName,
+        description: description,
+        site: author,
+        creator: author,
+    },
+};
 
 type Props = {
     children: React.ReactNode;
