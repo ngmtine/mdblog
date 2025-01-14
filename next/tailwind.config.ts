2fb8f7eb940264cfa7ee71b2559fae9be847e28a
ngmtine <ngmfries@gmail.com>
2025-01-14 11:08:26 +0900

    init  add: biome, postgres, daisyui

---
+++ next/tailwind.config.ts
@@ -0,0 +1,17 @@
+import daisyui from "daisyui";
+import type { Config } from "tailwindcss";
+
+export default {
+    content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+                "iceberg-dark": "#161821",
+                "iceberg-light": "#e8e9ec",
+            },
+        },
+    },
+    plugins: [daisyui],
+} satisfies Config;
