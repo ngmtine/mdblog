150983cfa923274cfb376b32da360cb6d639476e
ngmtine <ngmfries@gmail.com>
2025-01-20 14:20:53 +0900

    update: supabaseへの接続情報

--- next/app/util/db.ts
+++ next/app/util/db.ts
@@ -1,9 +1,25 @@
 import postgres from "postgres";
 
 export const db = postgres({
-    host: "localhost",
-    port: 5433,
-    database: "main",
-    username: "dockeruser",
-    password: "dockerpass",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
 });
+
+/*
+
+FIXME:
+supabaseの接続情報を渡す際、以下のように文字列渡すのが楽
+postgres(process.env.CONNECTION_STRING)
+
+が、以下のエラーが発生する
+[Error: SASL_SIGNATURE_MISMATCH: The server did not return the correct signature] {
+  code: 'SASL_SIGNATURE_MISMATCH'
+}
+
+おそらくだが、passwordの文字列が正しく渡っていない
+そのうち調べる
+
+*/
