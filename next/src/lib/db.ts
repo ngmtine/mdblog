5b85d3d01a88bbae8fe8e8a53514f396d9c2f886
ngmtine <ngmfries@gmail.com>
2025-06-20 09:19:57 +0900

    chore: .envにconnectionStringないとエラーにする

--- next/src/lib/db.ts
+++ next/src/lib/db.ts
@@ -1,15 +1,6 @@
 import postgres from "postgres";
 
 const connectionString = process.env.CONNECTION_STRING;
+if (!connectionString) throw new Error("connectionString is undefined");
 
-export const db = connectionString
-    ? // supabase
-      postgres(connectionString)
-    : // docker
-      postgres({
-          host: "localhost",
-          port: 5433,
-          database: "main",
-          user: "dockeruser",
-          password: "dockerpass",
-      });
+export const db = postgres(connectionString);
