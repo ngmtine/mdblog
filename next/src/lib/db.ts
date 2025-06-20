d02ef9dc2ae8a0912e674895944ed3b6dbfc490a
ngmtine <ngmfries@gmail.com>
2025-06-20 11:33:31 +0900

    fix: 開発サーバーでのdbコネクションプーリング枯渇の対応

--- next/src/lib/db.ts
+++ next/src/lib/db.ts
@@ -1,6 +1,23 @@
+import type { Sql } from "postgres";
 import postgres from "postgres";
 
+// グローバル空間に型追加
+declare global {
+    var db: Sql | undefined;
+}
+
+// connectionString取得、環境変数に無ければエラー
 const connectionString = process.env.CONNECTION_STRING;
 if (!connectionString) throw new Error("connectionString is undefined");
 
-export const db = postgres(connectionString);
+// dbインスタンスをキャッシュから取得または作成
+// biome-ignore lint: suspicious/noRedeclare
+const db = globalThis.db ?? postgres(connectionString);
+
+// 開発環境の場合、作成したインスタンスをキャッシュ
+if (process.env.NODE_ENV !== "production") {
+    globalThis.db = db;
+}
+
+// シングルトン返却
+export { db };
--- next/src/lib/executeQuery.ts
+++ next/src/lib/executeQuery.ts
@@ -1,4 +1,6 @@
 import type { SerializableParameter } from "postgres";
 import { db } from "./db";
 
-export const executeQuery = async <T>(query: string, params: SerializableParameter[] = []): Promise<T[]> => (await db.unsafe(query, params)) as T[];
+export const executeQuery = async <T>(query: string, params: SerializableParameter[] = []): Promise<T[]> => {
+    return (await db.unsafe(query, params)) as T[];
+};
